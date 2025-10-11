70f899902b8304c8ab553b25f5f4ef49b48512ec
ishikakaur64 <gagandeepkaur781@gmail.com>
2025-10-11 12:09:57 +0530

    Codingal

--- Codingal/Circle.py
+++ Codingal/Circle.py
@@ -1,1 +1,11 @@
 import turtle
+turtle.Screen().bgcolor("lightblue")
+
+circle = turtle.Turtle()
+circle.pencolor("red")
+circle.width(4)
+circle.speed(5)
+
+circle.circle(100) 
+
+turtle.done()
---
+++ Codingal/Circumference.py
@@ -0,0 +1,8 @@
+# Program to calculate the circumference of a circle
+
+def circumference(radius):
+    return 2 * 3.14 * radius  
+
+r = float(input("Enter the radius of the circle: "))
+
+print("The circumference of the circle is:", circumference(r))
