d1fc69cb59aefd69d97587c8567f6315a599efd7
ishikakaur64 <gagandeepkaur781@gmail.com>
2025-10-11 12:55:40 +0530

    Codingal

---
+++ Codingal/Check Age.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you?:"))
+if age >10:
+    if age>=10 and age<=20:
+        print("you are allowed in class age limit in between 10 and 20")
+    else:
+        print("you arw  not allowed in class")
+else:
+    print("you are too young for the class")
