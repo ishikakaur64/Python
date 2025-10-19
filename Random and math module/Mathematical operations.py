082619498833daf72583142defdca70599b2ac4f
ishikakaur64 <gagandeepkaur781@gmail.com>
2025-10-19 16:49:00 +0530

    Random and math module

---
+++ Random and math module/Mathematical operations.py
@@ -0,0 +1,8 @@
+import math
+print(math.sqrt(24))
+print(math.floor(46.4))
+print(math.ceil(12.5))
+print(math.factorial(5))
+print(math.pi)
+print(math.exp(2**3))
+print(math.pow(5,6))
---
+++ Random and math module/Number game.py
@@ -0,0 +1,15 @@
+import random
+playing = True
+number = str(random.randint(10,20))
+
+print("I will generate a number from 10 to 20, and you have to guess the number one digit at a time.")
+print("The game ends when you get 1 hero!")
+
+while playing:
+    guess = input("Give me you best guess!")
+    if number == guess:
+        print("you win the game")
+        print("the number was",number)
+
+    else:
+        print("Your guess isn't quite right,try again.")
